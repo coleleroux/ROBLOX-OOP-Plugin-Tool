62baed0aa5724241239b1b80ff37dfad84d4bd70
h4artful <37340031+coleleroux@users.noreply.github.com>
2022-03-01 18:19:01 -0800

    OOP structure in main script
    Added main basecode, and refactored into OOP

--- src/ObjectOrientedTool/Main/init.server.lua
+++ src/ObjectOrientedTool/Main/init.server.lua
@@ -15,103 +15,115 @@ local function Radial()
         Description = "Toggle the radial menu"
     }
     local function UI()
-        local function Create(): table
+        local function Get(): table
+            local Get = {}
+            local function Create(parameters: table): Instance
+                local Class = {}
+                Class.__type = parameters._type
+                Class._msg, Class.Object = pcall(parameters.Create)
+                if not Class._msg then -- <boolean> message is a handshake in a protected call.
+                    return warn("<Class> Object: "..tostring(parameters._type).." could not be created")
+                end
+                Class.__index = Object -- order of creation.
+                function Class:__tostring()
+                    return Class.__type
+                end
+                return Class -- return a metatable for better flexibility
+            end
             -- Scope Declarations
-            local screen, background, radial
+            local Screen, Background, Radial
             -- Vanilla Instances
-            local Screen = function(): Instance
-                local ScreenGui = Instance.new("ScreenGui")
-                ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-                ScreenGui.Parent = game:GetService("CoreGui")
-                return ScreenGui
-            end
-            local Background = function(screen_gui: Instance): Instance
-                -- Create a Background Frame
-                local bgFrame = Instance.new("Frame")
-                bgFrame.BackgroundColor3 = Color3.fromRGB(39,39,39)
-                bgFrame.BackgroundTransparency = .8
-                bgFrame.BorderSizePixel = 0;
-                bgFrame.ZIndex = -1
-                bgFrame.Active = false
-                bgFrame.Size = UDim2.fromScale(1,1)
-                bgFrame.Parent = DefaultRadial and screen_gui or nil
-                return bgFrame
-            end
-            -- EgoMoose UI Instances
+            Screen = Create {
+                _type = "Screen";
+                Create = function(self, ...)
+                    local ScreenGui = Instance.new("ScreenGui")
+                    ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+                    ScreenGui.Parent = game:GetService("CoreGui")
+                    return ScreenGui
+                end
+            }
+            Background = Create {
+                _type = "Background";
+                Create = function(self, ...)
+                    -- Create a Background Frame
+                    local bgFrame = Instance.new("Frame")
+                    bgFrame.BackgroundColor3 = Color3.fromRGB(39,39,39)
+                    bgFrame.BackgroundTransparency = .8
+                    bgFrame.BorderSizePixel = 0;
+                    bgFrame.ZIndex = -1
+                    bgFrame.Active = false
+                    bgFrame.Size = UDim2.fromScale(1,1)
+                    bgFrame.Parent = DefaultRadial and Screen.Object or nil
+                    return bgFrame
+                end
+            }
+            -- -- EgoMoose UI Instances
             local EgoMoose = require(script.GuiLib.LazyLoader)
-            local Radial = function(): Instance
-                local RadialClass = {}
-                RadialClass.__type = "Radial"
-                local RadialMenu = EgoMoose.Classes.RadialMenu.new(2, .75, math.pi*.5) -- create our radial menu
-                RadialMenu.Frame.AnchorPoint = Vector2.new(.5,.5)
-                RadialMenu.Frame.Position = UDim2.fromScale(.5,.5)
-                RadialMenu.Frame.Size = UDim2.fromScale(.3,.3)
-                RadialMenu.Frame.Parent = frame
-            
-                for i = 1, 2 do
-                    local label = Instance.new("ImageLabel")
-                    label.Size = UDim2.new(1, 0, 1, 0)
-                    label.BackgroundTransparency = 1
-                    -- label.Image = images[i].Texture
-                    label.Parent = RadialMenu:GetAttachment(i)
+            Radial = Create {
+                _type = "Radial";
+                Create = function(self, ...)
+                    local RadialMenu = EgoMoose.Classes.RadialMenu.new(2, .75, math.pi*.5) -- create our radial menu
+                    RadialMenu.Frame.AnchorPoint = Vector2.new(.5,.5)
+                    RadialMenu.Frame.Position = UDim2.fromScale(.5,.5)
+                    RadialMenu.Frame.Size = UDim2.fromScale(.3,.3)
+                    RadialMenu.Frame.Parent = Background.Object
+                
+                    for i = 1, 2 do
+                        local label = Instance.new("ImageLabel")
+                        label.Size = UDim2.new(1, 0, 1, 0)
+                        label.BackgroundTransparency = 1
+                        -- label.Image = images[i].Texture
+                        label.Parent = RadialMenu:GetAttachment(i)
+                    end
+                    return RadialMenu
                 end
-                RadialClass.__index = RadialMenu
-                function RadialClass:__tostring()
-                    return RadialClass.__type
-                end
-                return setmetatable({},RadialClass)
-            end
-            return {Screen,Background,Radial} -- order of creation.
+            }
+            Get = {Screen, Background, Radial}
+            return Get
             -- End of Create Scope
         end
         local function Initialize(self): table
-            local created = self:Create()
-            local new = setmetatable({__index = self}, created)
+            local empty = {} -- storage table,
+            local created = self:Get()
+            local self = setmetatable({__index = self}, created) -- override self
             -- enumerate through self,
-            for name,func in next, getmetatable(new) do
-                local message, child = pcall(func)
-                if message then -- <boolean> message is a handshake in a protected call.
-                    new[tostring(child)] = child
+            for index,meta in next, getmetatable(self) do
+                if meta then
+                    empty[meta.__type] = meta.Object
                 end
             end
-
-            warn(new.Radial)
-
-            return new
+            return empty
         end
-        return {Create = Create, Initialize = Initialize}
+        return {Get = Get, Initialize = Initialize}
     end
 
     local UI = UI():Initialize()
-    -- table.foreach(UI, print)
-
-
     -- ToggleRadial <Button> Connections
     ToggleRadial.Object.Click:Connect(function()
         DefaultRadial = not DefaultRadial and true
-        frame.Parent = DefaultRadial and ScreenGui or nil
+        UI.Background.Parent = DefaultRadial and UI.Screen or nil
     end)
 
 
-    RadialMenu.Clicked:Connect(function(index)
+    UI.Radial.Clicked:Connect(function(index)
         print(index)
     end)
 
-    RadialMenu.Hover:Connect(function(oldIndex, newIndex)
+    UI.Radial.Hover:Connect(function(oldIndex, newIndex)
         if (oldIndex) then
-            RadialMenu:GetRadial(oldIndex).ImageColor3 = Color3.new(0, 0, 0)
+            UI.Radial:GetRadial(oldIndex).ImageColor3 = Color3.new(0, 0, 0)
         end
-        RadialMenu:GetRadial(newIndex).ImageColor3 = Color3.new(1, 1, 1)
+        UI.Radial:GetRadial(newIndex).ImageColor3 = Color3.new(1, 1, 1)
     end)
 
     plugin.Unloading:Connect(function()
-        RadialMenu:Destroy()
-        frame:Destroy()
+        UI.Radial:Destroy()
+        UI.Background:Destroy()
     end)
 
     local GUI = game:GetService("GuiService")
     game["Run Service"].Stepped:Connect(function(dt)
-        RadialMenu.Enabled = not GUI.SelectedObject
+        UI.Radial.Enabled = not GUI.SelectedObject
     end)
 end
 Radial()
